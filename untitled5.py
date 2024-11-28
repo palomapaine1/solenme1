843430ec6c321ff8e2afd04bd5dbdadcb11ac87f
palomapaine1 <palomapaine4@gmail.com>
2024-11-28 09:08:22 -0300

    Update untitled5.py

--- untitled5.py
+++ untitled5.py
@@ -13,17 +13,5 @@ import streamlit as st
 
 # Título de la aplicación
 st.title('Aplicación Web: Datos desde una API REST')
-# URL de la API REST (puedes cambiarla por cualquier API pública que devuelva JSON)
-api_url = 'https://jsonplaceholder.typicode.com/posts'
-# Realizar la petición a la API
-response = requests.get(api_url)
 # Verificar que la respuesta sea exitosa (código 200)
-if response.status_code == 200:
-    # Convertir los datos JSON en un DataFrame de Pandas
-    data = response.json()
-    df = pd.DataFrame(data)
-# Mostrar los primeros registros
-    st.write('Datos obtenidos de la API:')
-    st.write(df.head())
-else:
-    st.error('Error al obtener los datos de la API')
+df= pd.read_csv('database_titanic(1)
