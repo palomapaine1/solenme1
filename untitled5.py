77e8c43b9416041b6d640a30c6d2d1b01b033478
palomapaine1 <palomapaine4@gmail.com>
2024-11-28 09:03:55 -0300

    Update untitled5.py

--- untitled5.py
+++ untitled5.py
@@ -18,12 +18,12 @@ api_url = 'https://jsonplaceholder.typicode.com/posts'
 # Realizar la petición a la API
 response = requests.get(api_url)
 # Verificar que la respuesta sea exitosa (código 200)
- if response.status_code == 200:
-# Convertir los datos JSON en un DataFrame de Pandas
-data = response.json()
- df = pd.DataFrame(data)
+if response.status_code == 200:
+    # Convertir los datos JSON en un DataFrame de Pandas
+    data = response.json()
+    df = pd.DataFrame(data)
 # Mostrar los primeros registros
-st.write('Datos obtenidos de la API:')
-st.write(df.head())
- else:
-st.error('Error al obtener los datos de la API')
+    st.write('Datos obtenidos de la API:')
+    st.write(df.head())
+else:
+    st.error('Error al obtener los datos de la API')
